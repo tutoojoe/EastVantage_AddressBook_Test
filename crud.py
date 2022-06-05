52e5db4a17916a163a47927f11c7aa1350957ca2
Tutoo Joe Philip <tutoojoephilip@gmail.com>
2022-06-05 16:05:07 +0530

    comments and error handling

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy import not_
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import not_, select
+from fastapi import HTTPException
 
 
 import models
@@ -11,34 +12,41 @@ from geopy.distance import geodesic
 
 
 def get_location(city_name):
+    """Returns the GeoLocation (Address, Latitude and Longitude of a given place."""
     loc = Nominatim(user_agent="GetLoc")
     getLoc = loc.geocode(city_name)
-
-    def get_coordinates(city_name):
-
-        return getLoc.latitude, getLoc.longitude
     return getLoc
 
 
 def get_coordinates(city_name):
+    """Returns a tuple with location co-ordinates (latitude, longitude) of the given city name."""
     loc = Nominatim(user_agent="GetLoc")
     getLoc = loc.geocode(city_name)
     return getLoc.latitude, getLoc.longitude
 
 
 def get_user(db: Session, user_id: int):
+    """Returns the User based on the given id"""
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
+def get_address(db: Session, address_id: int):
+    """Returns the Address based on the given id"""
+    return db.query(models.AddressBook).filter(models.AddressBook.id == address_id).first()
+
+
 def get_user_by_email(db: Session, email: str):
+    """Returns a the user based on the email id"""
     return db.query(models.User).filter(models.User.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
+    """Returns a list of all the Users"""
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
+    """Creates a User in the database"""
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(
         email=user.email, hashed_password=fake_hashed_password)
@@ -49,18 +57,25 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 
 def get_addresses(db: Session, skip: int = 0, limit: int = 100):
+    """Returns a list of all saved addresses"""
     return db.query(models.AddressBook).offset(skip).limit(limit).all()
 
 
 def create_user_address(db: Session, address: schemas.AddressBookCreate, user_id: int):
+    """Create an address submitted by the user"""
     location = get_location(address.city)
-    address.latitude = str(location.latitude)
-    address.longitude = str(location.longitude)
-    db_address = models.AddressBook(**address.dict(), owner_id=user_id)
-    db.add(db_address)
-    db.commit()
-    db.refresh(db_address)
-    return db_address
+    if location:
+        print(location, "location found")
+        address.latitude = str(location.latitude)
+        address.longitude = str(location.longitude)
+        db_address = models.AddressBook(**address.dict(), owner_id=user_id)
+        db.add(db_address)
+        db.commit()
+        db.refresh(db_address)
+        return db_address
+    else:
+        raise HTTPException(
+            status_code=404, detail="Please enter a valid city name")
 
 
 def calculate_distance(city1: str, city2: str):
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """Creates a user in database"""
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -32,12 +33,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """Returns a list of all the users in the database"""
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
+    """Returns the user based on the given ID"""
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -48,24 +51,27 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 def create_address_for_user(
     user_id: int, address: schemas.AddressBookCreate, db: Session = Depends(get_db)
 ):
+    """Creates an address for the user. A user can create multiple addresses"""
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     return crud.create_user_address(db=db, address=address, user_id=user_id)
 
 
 @app.get("/addresses/", response_model=list[schemas.AddressBook])
 def read_addresses(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """Returns a list of all the addresses in the database"""
     addresses = crud.get_addresses(db, skip=skip, limit=limit)
     return addresses
 
 
-@app.post("/distance/")
-def calculate_distance(city: str, city1: str, db: Session = Depends(get_db)):
-    distance = crud.get_nearby_locations(db, city, city1)
-    return distance
-
-
 @app.post("/nearby_addresses/")
 def find_nearby_addresses(address_id: int, distance_in_km: int, db: Session = Depends(get_db)):
+    """Returns a list of all the nearby addresses in the database, within the given distance parameter"""
+    db_address = crud.get_address(db, address_id=address_id)
+    if db_address is None:
+        raise HTTPException(
+            status_code=404, detail="The given address id is not found. Pleases re-enter.")
     nearby_addresses = crud.get_nearby_addresses(
         address_id, distance_in_km, db)
-
     return nearby_addresses
